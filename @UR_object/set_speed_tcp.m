eb0546c0a5b9627f37cc54ece4c282d2b0ba3d8b
Shijie Zhu <56151338+zhusj16@users.noreply.github.com>
2020-02-13 19:21:53 +0800

    Update set_speed_tcp.m
    transformation of the speed vector is implemented with build-in ur functions. Previously this transformaion is performed in matlab, which needs to refresh the pose first and may cost extra time

--- @UR_object/set_speed_tcp.m
+++ @UR_object/set_speed_tcp.m
@@ -1,8 +1,7 @@
 function cmd = set_speed_tcp(obj,varargin)
-% servo_cart，在机器人基坐标系中以给定速度V运动
 % v: [x,y,z,rx,ry,rz],xyz单位为m/s，rx,ry,rz单位为rad/s
 
-if strcmp(obj.s2.status,'closed')   %如果没打开端口，则打开之
+if strcmp(obj.s2.status,'closed')   % 如果没打开端口，则打开之
     fopen(obj.s2);
 end
 
@@ -15,14 +14,25 @@ else
 end
 
 t=100;
-R_robot = obj.Rxyz2R(obj.pose(4:6));
-v(1:3) = v(1:3)*R_robot';
-v(4:6) = v(4:6)*R_robot';
-
-cmd = sprintf(...
-     'speedl([%f,%f,%f,%f,%f,%f],%f,%f)\n'...
+  
+cmd_ = sprintf(...
+     ' speedl(wrench_trans(p[0,0,0,pose[3],pose[4],pose[5]],[%f,%f,%f,%f,%f,%f]),%f,%f)\n'...
       ,v,a,t);
+
+cmd =            'def speed_tcp():\n';
+cmd = strcat(cmd,'\t pose = get_target_tcp_pose()\n');
+cmd = strcat(cmd,'\t',cmd_,'\n');
+cmd = strcat(cmd,'end\n');
+cmd = sprintf(cmd);
   
 if nargout==0
     fprintf(obj.s2,cmd);
+else
+    % this is for force mode
+    cmd =            'def speed_tcp():\n';
+    cmd = strcat(cmd,'\t\t pose = get_target_tcp_pose()\n');
+    cmd = strcat(cmd,'\t\t',cmd_,'\n');
+    cmd = strcat(cmd,'\tend\n');
+    cmd = strcat(cmd,'\tspeed_tcp()');
+    cmd = sprintf(cmd);
 end
